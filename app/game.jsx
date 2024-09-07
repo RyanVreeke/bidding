453875f6542785a72e76a65791232e64e5791407
Ryan Vreeke <rvreeke@michiganlabs.com>
2024-09-07 13:05:04 -0400

    feat: Basic app setup.

---
+++ app/game.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Game = () => {
+  return (
+    <View>
+      <Text>Game</Text>
+    </View>
+  )
+}
+
+export default Game
--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
-import "./global.css";
+import { Link } from "expo-router";
+import "../global.css";
 
 export default function App() {
 	return (
 		<View className="flex-1 items-center justify-center">
-			<Text className="text-3xl text-center p-4">
-				Open up App.tsx to start working on your app!
-			</Text>
+			<Text className="text-3xl text-center p-4">Bidding Card Game!</Text>
 			<StatusBar style="auto" />
+			<Link className="text-blue-500" href="/game">
+				Go to game!
+			</Link>
 		</View>
 	);
 }
